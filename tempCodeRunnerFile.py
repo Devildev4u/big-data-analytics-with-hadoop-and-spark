f8c450df93df496626190120c9ca632a20ce8ac7
DEVESH KUMAR <159315557+Devildev4u@users.noreply.github.com>
2025-05-06 23:38:51 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-
-    df_transformed = process_data(df)
-    predictions = train_model(df_transformed)
-    analyze_data(predictions)
-    visualize_results(predictions)
-    generate_report(predictions)
-    logging.info("Pipeline finished.")
