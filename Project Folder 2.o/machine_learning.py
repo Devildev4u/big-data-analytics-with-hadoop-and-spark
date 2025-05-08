e4fcb19885d309a94aba1089f1c0da77de901545
DEVESH KUMAR <159315557+Devildev4u@users.noreply.github.com>
2025-05-08 13:23:03 +0530

    Add files via upload

--- Project Folder 2.o/machine_learning.py
+++ Project Folder 2.o/machine_learning.py
@@ -1,7 +1,7 @@
 from pyspark.ml.regression import LinearRegression
 
 def train_model(df_transformed):
-    lr = LinearRegression(featuresCol="features", labelCol="feature1")
+    lr = LinearRegression(featuresCol="features", labelCol="feature1", regParam=0.1)
     model = lr.fit(df_transformed)
     predictions = model.transform(df_transformed)
     return predictions
